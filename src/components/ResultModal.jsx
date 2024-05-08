62ba66f8564f5a6a6bea259f8401678368852d92
abhishek <nehabhrit@gmail.com>
2024-05-08 16:42:33 +0530

    Second commit

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,20 +1,28 @@
 import { forwardRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime },ref) {
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const userLost = remainingTime <= 0;
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={ref} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You Lost</h2>}
       <p>
         The taget time was <strong>{targetTime}</strong> seconds.
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
   );
-})
+});
 
 export default ResultModal;
